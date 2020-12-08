ce00855b1dd2429aa1b0b2edb9bb548af42e9e30
theabbie <abhishek7gg7@gmail.com>
2020-12-08 23:41:08 +0530

    Initialized Repo

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# tenor
-Unofficial Tenor API
+# Tenor API
+
+![Tenor](https://user-images.githubusercontent.com/17960677/101523435-9c0b2180-39ae-11eb-9544-b83cceaf6457.png)
+
+>**Unofficial Tenor API**
+
+## Contributing
+
+Thank you for your interest in contributing, If you feel like there's something missing or any new feature can be added, just create a PR and I will see the rest.
+
+## Help
+
+You can contact me on social media, Everything about me can be found [here](https://theabbie.github.io)
+
+## Installation
+
+### Requirements
+
+* Node.Js installed
+
+### Dev Dependencies
+
+* Axios
+* Form-Data
+
+## Credits
+
+* [Tenor](https://tenor.com) For Creating an excellent Platform.
+
+## Contact
+
+Contact me anywhere, just visit [my portfolio](https://theabbie.github.io)
+
+## License
+
+This project is licensed under MIT License, See [LICENSE](/LICENSE) for more information
+
---
+++ index.js
@@ -0,0 +1,44 @@
+var axios = require("axios");
+var fd = require("form-data");
+var fs = require("fs");
+
+module.exports = class Tenor {
+  constructor() {}
+
+  async login(username,password) {
+    this.username = username;
+    this.password = password;
+    var token = await axios({
+      url: 'https://www.riffsy.com/keyboard.login?username='+this.username+'&password='+this.password
+    });
+    this.token = token.data.auth.token;
+    this.uid = token.data.user.id;
+    return token;
+  }
+
+  async addToken(token) {
+    this.token = token;
+    var uid = await axios({
+      url: 'https://www.riffsy.com/keyboard.user?token='+this.token
+    });
+    this.uid = uid.data.user.id;
+    return uid.data;
+  }
+
+  async upload(path,tags) {
+    var data = new fd();
+    data.append("token",this.token);
+    data.append("collectionid","keyboarduser-"+this.uid);
+    data.append("streams","savedriffs");
+    data.append("tags",tags || "gif");
+    data.append("platform","android");
+    data.append("file",fs.createReadStream(path));
+    var result = await axios({
+      url: 'https://www.riffsy.com/keyboard.uploadgifandroid',
+      method: 'POST',
+      data: data,
+      headers: data.getHeaders()
+    });
+    return result.data;
+  }
+}
